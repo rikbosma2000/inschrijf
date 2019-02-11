67ee9feed4d68767624d09abc4c5fdecf5698510
rikbosma2000 <44288685+rikbosma2000@users.noreply.github.com>
2019-02-11 10:01:10 +0100

    Add files via upload

--- js/cms.js
+++ js/cms.js
@@ -6,6 +6,8 @@ var countVerhuur = 1;
 
 //zelf vervoer in cms
 
+
+
 $(document).on('click', ".addVervoer", function (e) {
 
     var vervoerAppend = '<div class="box" style="margin-top: 10px;">\
@@ -16,13 +18,13 @@ $(document).on('click', ".addVervoer", function (e) {
 									</div>\
 								</div>\
 								<div class="col-md-6">\
-									<input type="text" class="form-control" name="vervoer[]" placeholder="Vervoer zelf" class="vervoer" id="vervoer">\
+									<input type="text" class="form-control InputFontChangers" name="vervoer[]" placeholder="Vervoer zelf" class="vervoer" id="vervoer">\
 								</div>\
 								<div class="col-md-4 input-group">\
 								    <div class="input-group-prepend">\
 									    <span class="input-group-text" id="basic-addon1">€</span>\
 									</div>\
-									<input type="text" class="form-control" placeholder="costs" name="vervoer_costs[]">\
+									<input type="text" class="form-control InputFontChangers" placeholder="Kosten" name="vervoer_costs[]">\
 								</div>\
 							</div>\
 						</div>';
@@ -50,13 +52,13 @@ $(".addEditie").click(function () {
 																</div>\
 															</div>\
 															<div class="col-md-6">\
-																<input type="text" class="form-control" placeholder="Editie" name="editie[]">\
+																<input type="text" class="form-control InputFontChangers" placeholder="Editie" name="editie[]">\
 															</div>\
 															<div class="col-md-4 input-group">\
 																<div class="input-group-prepend">\
 																	<span class="input-group-text" id="basic-addon2">€</span>\
 																</div>\
-																<input type="text" class="form-control" placeholder="costs" name="editie_costs[]">\
+																<input type="text" class="form-control InputFontChangers" placeholder="Kosten" name="editie_costs[]">\
 															</div>\
 														</div>';
 
@@ -89,7 +91,7 @@ $(".addAccomodatie").click(function () {
 																<div class="input-group-prepend">\
 																	<span class="input-group-text" id="basic-addon3">€</span>\
 																</div>\
-																<input type="text" class="form-control" placeholder="costs" name="accomodatie_costs[]">\
+																<input type="text" class="form-control InputFontChangers" placeholder="Kosten" name="accomodatie_costs[]">\
 															</div>\
 														</div>');
 });
@@ -115,13 +117,13 @@ $(".addVerhuur").click(function () {
 																</div>\
 															</div>\
 															<div class="col-md-6">\
-																<input type="text" class="form-control" placeholder="Verhuur" name="verhuur[]">\
+																<input type="text" class="form-control InputFontChangers" placeholder="Verhuur" name="verhuur[]">\
 															</div>\
 															<div class="col-md-4 input-group">\
 																<div class="input-group-prepend">\
 																	<span class="input-group-text" id="basic-addon4">€</span>\
 																</div>\
-																<input type="text" class="form-control" placeholder="costs" name="verhuur_costs[]">\
+																<input type="text" class="form-control InputFontChangers" placeholder="Kosten" name="verhuur_costs[]">\
 															</div>\
 														</div>');
 });
@@ -132,4 +134,58 @@ $(document).on('click', '.minVerhuur', function (e) {
     }
 
 });
+
+// ajax function ensures that data gets fetched
+// ajax call should fire when the document is ready
+function getAjaxRetrieveEDIT() {
+	$.ajax({
+		url: 'processEdit.php',
+		type: 'GET',
+		data: '',
+		success: function(response) {
+			response = JSON.parse(response);
+			$('#evenement').val(response[0].evenement);
+			$('#datum_begin').val(response[0].datum_begin);
+			$('#datum_eind').val(response[0].datum_eind);
+			$('#Prijs').val(response[0].prijs);
+			$('#max_deelname').val(response[0].max_deelnemers);
+			$('#table_name').val(response[0].table_name);
+			console.log(response);
+		}
+	});
+}
+
+//this function ensures the database table cell doesn't contain any whitespace
+// it trims the whitespace from the main title and inserts it concat as a table cell
+function getDuplicateText() {
+	$('#evenement').keyup(function() {
+		let $this = $(this).val();
+		$('#table_name').val($this);
+		let tableName = $('#table_name').val();
+		let x = tableName.replace(/\s/g, "");
+		console.log(x);
+		$('#table_name').val(x);
+	});
+}
+
+function clearBootstrapStandardModal() {
+		$('#cmsInstructions').modal({
+			backdrop: 'static',
+			keyboard: false
+		});
+}
+//document.ready starts below, to ensure some scripts do NOT fail
+// if your script DOES fail, try it with the document.ready, to ensure the script has been loaded properly
+//NOTE: the script is being loaded AFTER the HTML, at the bottom near rule 265
+//therefore the document.ready is optional and is NOT required, however it IS a safety measurement
+	$(function() {
+		if(window.location.href.indexOf("edit") > -1) {
+			getAjaxRetrieveEDIT();
+		} else {
+			// the else statement was used for a console.log event
+			// the else statement has been kept in case needed for later use
+		}
+	});
+
+
 
